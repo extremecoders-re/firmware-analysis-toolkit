af354e6337ee07944d6d9dd1da392fcc3d8aada5
Extreme Coders <extremecoders@hotmail.com>
2017-11-18 02:49:42 +0530

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # firmware-analysis-toolkit
+
+FAT is a toolkit built in order to help security researchers analyze and identify vulnerabilities in IoT and embedded device firmware. This is built in order to use for the "*[Offensive IoT Exploitation](http://offensiveiotexploitation.com/)*" training conducted by [Attify](https://attify.com). 
+
+This is a (minor) rewrite of original repo available at https://github.com/attify/firmware-analysis-toolkit.
+
+## Installation instructions
+
+1. Install `pexpect`
+
+    `pip2 install pexpect`
+    
+2. Install firmadyne as described in https://github.com/firmadyne/firmadyne
+
+3. Copy over `fat.py` and `reset.sh` to the directory where you installed firmadyne
+
+4. Edit `fat.py` and adjust the paths to firmadyne and binwalk. Additionally, provide the root password as well. Firmadyne requires root privileges for some of its operations. The root password is provided in the script itself to automate the process.
+
+    ```python
+    # Configurations - change this according to your system
+    firmadyne_path = "/home/ec/firmadyne"
+    binwalk_path = "/usr/local/bin/binwalk"
+    root_pass = "root"
+    firmadyne_pass = "firmadyne"
+    ```
+    
+## Usage ?
+
+Provide the firmware filename as an argument to the script. If not provided, the script would prompt for it at runtime.
+
+```
+$ ./fat.py <firmware binary>
+```
+
+To remove all analyzed firmware images, run
+
+```
+$ ./reset.sh
+```
+## Example
+
+```
+$ ./fat-new.py DIR850LB1_FW210WWb03.bin 
+
+                               __           _   
+                              / _|         | |  
+                             | |_    __ _  | |_ 
+                             |  _|  / _` | | __|
+                             | |   | (_| | | |_ 
+                             |_|    \__,_|  \__|                    
+                    
+                Welcome to the Firmware Analysis Toolkit - v0.2
+    Offensive IoT Exploitation Training  - http://offensiveiotexploitation.com
+                  By Attify - https://attify.com  | @attifyme
+    
+[?] Enter the name or absolute path of the firmware you want to analyse : DIR850LB1_FW210WWb03.bin
+[?] Enter the brand of the firmware : dlink
+[+] Now going to extract the firmware. Hold on..
+[+] Firmware : DIR850LB1_FW210WWb03.bin
+[+] Brand : dlink
+[+] Database image ID : 1
+[+] Identifying architecture
+[+] Architecture : mipseb
+[+] Storing filesystem in database
+[!] Filesystem already exists
+[+] Building QEMU disk image
+[+] Setting up the network connection, please standby
+[+] Network interfaces : [('br0', '192.168.0.1'), ('br1', '192.168.7.1')]
+[+] Running the firmware finally
+[+] command line : sudo /home/ec/firmadyne/scratch/1/run.sh
+[*] Press ENTER to run the firmware...
+```
---
+++ fat.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python2.7
+
+import os
+import pexpect
+import sys
+import subprocess
+
+# Put this script in the firmadyne path downloadable from
+# https://github.com/firmadyne/firmadyne
+
+#Configurations - change this according to your system
+firmadyne_path = "/home/ec/firmadyne"
+binwalk_path = "/usr/local/bin/binwalk"
+root_pass = "root"
+firmadyne_pass = "firmadyne"
+
+
+def show_banner():
+    print """
+                               __           _   
+                              / _|         | |  
+                             | |_    __ _  | |_ 
+                             |  _|  / _` | | __|
+                             | |   | (_| | | |_ 
+                             |_|    \__,_|  \__|                    
+                    
+                Welcome to the Firmware Analysis Toolkit - v0.2
+    Offensive IoT Exploitation Training  - http://offensiveiotexploitation.com
+                  By Attify - https://attify.com  | @attifyme
+    """
+
+
+def get_info():
+    if len(sys.argv) == 2:
+        firm_name = sys.argv[1]
+        print "[?] Enter the name or absolute path of the firmware you want to analyse : " + firm_name
+    else:
+        firm_name = raw_input("[?] Enter the name or absolute path of the firmware you want to analyse : ")
+    firm_brand = raw_input("[?] Enter the brand of the firmware : ")
+    return (firm_name, firm_brand)
+
+
+def run_extractor(firm_name, firm_brand):
+    print "[+] Now going to extract the firmware. Hold on.."
+    print "[+] Firmware : " + firm_name
+    print "[+] Brand : " + firm_brand
+    extractor_cmd = firmadyne_path + "/sources/extractor/extractor.py -b " + firm_brand + " -sql 127.0.0.1 -np -nk " + "\""+ firm_name + "\"" + " images "
+    child = pexpect.spawn(extractor_cmd)
+    child.expect("Database Image ID: ")
+    image_id = child.readline().strip()
+    print "[+] Database image ID : " + image_id
+    child.expect(pexpect.EOF)
+    return image_id
+    
+
+def identify_arch(image_id):
+    print "[+] Identifying architecture"
+    identfy_arch_cmd = firmadyne_path + "/scripts/getArch.sh ./images/" + image_id + ".tar.gz"
+    child = pexpect.spawn(identfy_arch_cmd)
+    child.expect(":")
+    arch = child.readline().strip()
+    print "[+] Architecture : " + arch
+    child.expect("Password for user firmadyne: ")    
+    child.sendline(firmadyne_pass)
+    child.expect(pexpect.EOF)
+    return arch
+    
+
+def tar2db(image_id):
+    print "[+] Storing filesystem in database"
+    tar2db_cmd = firmadyne_path + "/scripts/tar2db.py -i " + image_id + " -f " + firmadyne_path + "/images/" + image_id + ".tar.gz"
+    output_tar2db = pexpect.run(tar2db_cmd)
+
+    if "already exists" in output_tar2db:
+        print "[!] Filesystem already exists"
+
+
+def make_image(arch, image_id):
+    print "[+] Building QEMU disk image"
+    makeimage_cmd = "sudo " + firmadyne_path + "/scripts/makeImage.sh " + image_id + " " + arch
+    child = pexpect.spawn(makeimage_cmd)
+    child.sendline(root_pass)
+    child.expect(pexpect.EOF)
+
+
+def setup_network(arch, image_id):
+    print "[+] Setting up the network connection, please standby"
+    network_cmd = "sudo " + firmadyne_path + "/scripts/inferNetwork.sh " + image_id + " " + arch
+    child = pexpect.spawn(network_cmd)
+    child.sendline(root_pass)
+    child.expect("Interfaces:", timeout=120)
+    interfaces = child.readline().strip()
+    print "[+] Network interfaces : " + interfaces
+    child.expect(pexpect.EOF)
+
+
+def final_run(image_id):
+    print "[+] Running the firmware finally"
+    run_cmd = "sudo " + firmadyne_path + "/scratch/" + image_id + "/run.sh"
+    print "[+] command line : " + run_cmd
+    raw_input("[*] Press ENTER to run the firmware...")
+    child = pexpect.spawn(run_cmd)
+    child.sendline(root_pass)
+    child.interact()    
+
+
+def main():
+    show_banner()
+    firm_name, firm_brand = get_info()
+    image_id = run_extractor(firm_name, firm_brand)
+    
+    if image_id == "":
+        print "[!] Something went wrong"
+    else:
+        arch = identify_arch(image_id)        
+        tar2db(image_id)
+        make_image(arch, image_id)        
+        setup_network(arch, image_id)        
+        final_run(image_id)
+
+
+if __name__ == "__main__":
+    main()
